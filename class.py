92c7f18cb99539d05696c145121b686860a1787a
Madeleinesj <m.marchant-daisley@hotmail.com>
2023-01-28 16:48:44 -0800

    Create class.py

---
+++ class.py
@@ -0,0 +1,30 @@
+
+class employee:
+    title: ""
+    pay: ""
+    def setTitle(self, title):
+        self.title = title
+    def setPay(self, pay):
+        self.pay = pay
+
+class customer:
+    savings = ""
+    rating = ""
+    def setSavings(self, savings):
+        self.savings
+    def setRating(self, rating):
+        self.rating
+
+class people_In_Safeway(employee, customer):
+    experience = ""
+    def setExperience(self, experience):
+        self.experience
+
+
+Jim = employee()
+Jim.setTitle("Return customer")
+Jim.setPay("5000000")
+
+
+
+    
